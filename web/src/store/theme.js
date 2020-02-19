732799f520c7c3db845458ed7d8728c37b2e2254
gabewomble <gwomble27@gmail.com>
2020-02-19 09:00:30 -0600

    default theme to light

--- web/src/store/theme.js
+++ web/src/store/theme.js
@@ -1,4 +1,4 @@
 import { writable } from 'svelte/store';
 import { THEMES } from '../constants/store';
 
-export default writable(new Date().getHours() >= 18 ? THEMES.DARK : THEMES.LIGHT);
+export default writable(THEMES.LIGHT);
